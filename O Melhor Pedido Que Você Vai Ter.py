f00bb3ef114ffd1506a926ced972826476f2f370
José Willyan <willyansilvagomes2020@gmail.com>
2020-03-30 17:35:08 -0300

    Ela aceitou

---
+++ O Melhor Pedido Que Você Vai Ter.py
@@ -0,0 +1,30 @@
+print('Mô, eu sei que o pedido anterior não foi como você esperava, por isso que eu estudei um pouco mais essa linguagem\ne agora fiz um programa melhor. Espero que goste!')
+print("   ")
+print('Paixão da minha vida, você chegou rapidamente no meu coração, quando reparei já estava preso no amor. Obrigado por ser você!')
+print(     )
+resposta = str(input('Você aceita namorar comigo mesmo com toda a minha chatice pra limpar o quarto? Sim ou não? '))
+print(   )
+if resposta == 'sim':
+    print('Você é a maçã mais linda do pomar, a pessoa mais apaixonante que eu conheço.\nEu não te mereço, és muito mais que uma mulher, és meu amor!\nObrigado por confiar em mim! TE AMOOOO!!!')
+else:
+    print('Me perdoe se não cumpri as expectativas, falhei com você, mas não quero perder sua amizade e irmandade.\nVou me despedindo triste, mas prometo que vou fazer o melhor por nós.')
+print(   )
+if resposta == 'sim':
+    r2 = str(input('Esse foi o melhor pedido que você recebeu? '))
+    if r2 == 'sim':
+        print(  )
+        print('Muito obrigado pela sua avaliação,\nprometo que sempre procurarei ser a melhor pessoa pra você.')
+    else:
+        print(   )
+        print('Não foi dessa vez, mas eu vou tentar melhorar!\nO bom é que você aceitou kkkkkkkk')
+    print(    )
+    print('VOCÊ É O AMOR DA MINHA VIDAAAA!')
+else:
+    print('Estou triste pela sua resposta, mas gostaria de saber se você gostou.')
+    r2 = str(input('Esse foi o melhor pedido que você recebeu? '))
+    if r2 == 'sim':
+        print(    )
+        print('Oxi, então por que você não aceitou? Sua palhaça!')
+    else:
+        print(    )
+        print('Não foi dessa vez, mas eu vou tentar melhorar.')
